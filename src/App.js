fa9dee77ea301a2d51c32841852e24289f0acacb
Shivakumar Badiger <sbadiger708@gmail.com>
2019-12-04 15:06:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,204 @@
+/* Let CRA handle linting for sample app */
+import React, { Component } from 'react';
+import Spinner from 'react-spinner';
+import classNames from 'classnames';
+
+import AccCore from 'opentok-accelerator-core';
+import 'opentok-solutions-css';
+
+import logo from './logo.svg';
+import './App.css';
+
+let otCore;
+const otCoreOptions = {
+  credentials: {
+    apiKey: 46195312,
+    sessionId: '1_MX40NjE5NTMxMn5-MTU3NTQ0ODg1MjMyNX5JK043NU5CQllRSTFkTnE1d3RPNDlJN3d-fg',
+    token: 'T1==cGFydG5lcl9pZD00NjE5NTMxMiZzaWc9NmM3OGMwMDdlNzgzMjBmMzE5NzQ3ODA1MjY1NDYyOGE0ODZkZDc4YzpzZXNzaW9uX2lkPTFfTVg0ME5qRTVOVE14TW41LU1UVTNOVFEwT0RnMU1qTXlOWDVKSzA0M05VNUNRbGxSU1RGa1RuRTFkM1JQTkRsSk4zZC1mZyZjcmVhdGVfdGltZT0xNTc1NDQ4ODY5Jm5vbmNlPTAuNTY2Nzg3MjgwNzk5OTI1MiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTc1NDUyNDY4JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9',
+  },
+  // A container can either be a query selector or an HTML Element
+  streamContainers(pubSub, type, data, stream) {
+    return {
+      publisher: {
+        camera: '#cameraPublisherContainer',
+        screen: '#screenPublisherContainer',
+      },
+      subscriber: {
+        camera: '#cameraSubscriberContainer',
+        screen: '#screenSubscriberContainer',
+      },
+    }[pubSub][type];
+  },
+  controlsContainer: '#controls',
+  packages: ['textChat', 'screenSharing', 'annotation'],
+  communication: {
+    callProperties: null, // Using default
+  },
+  textChat: {
+    name: ['David', 'Paul', 'Emma', 'George', 'Amanda'][Math.random() * 5 | 0], // eslint-disable-line no-bitwise
+    waitingMessage: 'Messages will be delivered when other users arrive',
+    container: '#chat',
+  },
+  screenSharing: {
+    extensionID: 'plocfffmbcclpdifaikiikgplfnepkpo',
+    annotation: true,
+    externalWindow: false,
+    dev: true,
+    screenProperties: {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%',
+      showControls: false,
+      style: {
+        buttonDisplayMode: 'off',
+      },
+      videoSource: 'window',
+      fitMode: 'contain' // Using default
+    },
+  },
+  annotation: {
+    absoluteParent: {
+      publisher: '.App-video-container',
+      subscriber: '.App-video-container'
+    }
+  },
+};
+
+/**
+ * Build classes for container elements based on state
+ * @param {Object} state
+ */
+const containerClasses = (state) => {
+  const { active, meta, localAudioEnabled, localVideoEnabled } = state;
+  const sharingScreen = meta ? !!meta.publisher.screen : false;
+  const viewingSharedScreen = meta ? meta.subscriber.screen : false;
+  const activeCameraSubscribers = meta ? meta.subscriber.camera : 0;
+  const activeCameraSubscribersGt2 = activeCameraSubscribers > 2;
+  const activeCameraSubscribersOdd = activeCameraSubscribers % 2;
+  const screenshareActive = viewingSharedScreen || sharingScreen;
+  return {
+    controlClass: classNames('App-control-container', { hidden: !active }),
+    localAudioClass: classNames('ots-video-control circle audio', { hidden: !active, muted: !localAudioEnabled }),
+    localVideoClass: classNames('ots-video-control circle video', { hidden: !active, muted: !localVideoEnabled }),
+    localCallClass: classNames('ots-video-control circle end-call', { hidden: !active }),
+    cameraPublisherClass: classNames('video-container', { hidden: !active, small: !!activeCameraSubscribers || screenshareActive, left: screenshareActive }),
+    screenPublisherClass: classNames('video-container', { hidden: !active || !sharingScreen }),
+    cameraSubscriberClass: classNames('video-container', { hidden: !active || !activeCameraSubscribers },
+      { 'active-gt2': activeCameraSubscribersGt2 && !screenshareActive },
+      { 'active-odd': activeCameraSubscribersOdd && !screenshareActive },
+      { small: screenshareActive }
+    ),
+    screenSubscriberClass: classNames('video-container', { hidden: !viewingSharedScreen || !active }),
+  };
+};
+
+const connectingMask = () =>
+  <div className="App-mask">
+    <Spinner />
+    <div className="message with-spinner">Connecting</div>
+  </div>;
+
+const startCallMask = start =>
+  <div className="App-mask">
+    <button className="message button clickable" onClick={start}>Click to Start Call </button>
+  </div>;
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      connected: false,
+      active: false,
+      publishers: null,
+      subscribers: null,
+      meta: null,
+      localAudioEnabled: true,
+      localVideoEnabled: true,
+    };
+    this.startCall = this.startCall.bind(this);
+    this.endCall = this.endCall.bind(this);
+    this.toggleLocalAudio = this.toggleLocalAudio.bind(this);
+    this.toggleLocalVideo = this.toggleLocalVideo.bind(this);
+  }
+
+  componentDidMount() {
+    otCore = new AccCore(otCoreOptions);
+    otCore.connect().then(() => this.setState({ connected: true }));
+    const events = [
+      'subscribeToCamera',
+      'unsubscribeFromCamera',
+      'subscribeToScreen',
+      'unsubscribeFromScreen',
+      'startScreenShare',
+      'endScreenShare',
+    ];
+
+    events.forEach(event => otCore.on(event, ({ publishers, subscribers, meta }) => {
+      this.setState({ publishers, subscribers, meta });
+    }));
+  }
+
+  startCall() {
+    otCore.startCall()
+      .then(({ publishers, subscribers, meta }) => {
+        this.setState({ publishers, subscribers, meta, active: true });
+      }).catch(error => console.log(error));
+  }
+
+  endCall() {
+    otCore.endCall();
+    this.setState({ active: false });
+  }
+
+  toggleLocalAudio() {
+    otCore.toggleLocalAudio(!this.state.localAudioEnabled);
+    this.setState({ localAudioEnabled: !this.state.localAudioEnabled });
+  }
+
+  toggleLocalVideo() {
+    otCore.toggleLocalVideo(!this.state.localVideoEnabled);
+    this.setState({ localVideoEnabled: !this.state.localVideoEnabled });
+  }
+
+  render() {
+    const { connected, active } = this.state;
+    const {
+      localAudioClass,
+      localVideoClass,
+      localCallClass,
+      controlClass,
+      cameraPublisherClass,
+      screenPublisherClass,
+      cameraSubscriberClass,
+      screenSubscriberClass,
+    } = containerClasses(this.state);
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>OpenTok Accelerator Core</h1>
+        </div>
+        <div className="App-main">
+          <div className="App-video-container">
+            { !connected && connectingMask() }
+            { connected && !active && startCallMask(this.startCall)}
+            <div id="cameraPublisherContainer" className={cameraPublisherClass} />
+            <div id="screenPublisherContainer" className={screenPublisherClass} />
+            <div id="cameraSubscriberContainer" className={cameraSubscriberClass} />
+            <div id="screenSubscriberContainer" className={screenSubscriberClass} />
+          </div>
+          <div id="controls" className={controlClass}>
+            <div className={localAudioClass} onClick={this.toggleLocalAudio} />
+            <div className={localVideoClass} onClick={this.toggleLocalVideo} />
+            <div className={localCallClass} onClick={this.endCall} />
+          </div>
+          <div id="chat" className="App-chat-container" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
